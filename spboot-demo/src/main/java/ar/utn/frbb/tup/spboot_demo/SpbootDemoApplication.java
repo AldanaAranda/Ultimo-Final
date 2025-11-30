ced49dea8647acc9f096eeca64dca1085c08be53
Aldana Aranda <aldanaaranda07@gmail.com>
2025-11-29 23:54:59 -0300

    Inicializacion del proyecto con Spring Boot

---
+++ spboot-demo/src/main/java/ar/utn/frbb/tup/spboot_demo/SpbootDemoApplication.java
@@ -0,0 +1,13 @@
+package ar.utn.frbb.tup.spboot_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbootDemoApplication.class, args);
+	}
+
+}
---
+++ spboot-demo/src/test/java/ar/utn/frbb/tup/spboot_demo/SpbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.utn.frbb.tup.spboot_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
